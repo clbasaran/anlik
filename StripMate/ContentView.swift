b7c3183f74328d424a5f806c49e82c2c2ebdd63a
celal <celalba78@icloud.com>
2026-02-26 01:55:21 +0300

    Initial Commit

---
+++ StripMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StripMate
+//
+//  Created by Celal Başaran on 26.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StripMate/StripMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StripMateApp.swift
+//  StripMate
+//
+//  Created by Celal Başaran on 26.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct StripMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
